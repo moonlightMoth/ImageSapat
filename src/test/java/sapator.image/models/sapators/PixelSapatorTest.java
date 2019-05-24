abdd2440e48c8195a685929c259cb228a3669ddb
Rinefica <rinefica@gmail.com>
2019-05-24 11:29:08 +0300

    pixelSapator test ver1

--- src/test/java/sapator.image/models/sapators/PixelSapatorTest.java
+++ src/test/java/sapator.image/models/sapators/PixelSapatorTest.java
@@ -1,32 +1,75 @@
 package sapator.image.models.sapators;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import sapator.image.models.ImageParams;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.awt.image.DataBuffer;
+import java.io.*;
+
+import org.apache.commons.io.FileUtils;
+import sapator.image.models.ObservableBufferedImage;
 
 public class PixelSapatorTest
 {
     @Test
-    public void testInversion() {
+    public void testInversion() throws IOException {
         BufferedImage img = null;
-        try
-        {
-            img = ImageIO.read(
-                new FileInputStream(ImageParams.getInstance().PIC_START));
 
-            ByteArrayOutputStream bao = new ByteArrayOutputStream();
-            ImageIO.write(img,"jpg", bao);
-            ImageParams.getInstance().setBufferedImage(img);
+        ObservableBufferedImage obs = new ObservableBufferedImage(img);
 
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        PixelSapator.brightSapator.doSapat();
+        obs.addListener(() -> {});
+
+        ImageParams.getInstance().setObservableBufferedImage(obs);
+
+        img = ImageIO.read(
+            new FileInputStream("./src/main/resources/rose.jpg"));
+
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        ImageIO.write(img,"jpg", bao);
+        ImageParams.getInstance().setBufferedImage(img);
+
+        PixelSapator.invSapator.doSapat();
+        PixelSapator.invSapator.doSapat();
+
+        File temp = new File("./src/main/resources/temp.jpg");
+        ImageIO.write(
+            ImageParams.getInstance()
+                .getObservableBufferedImage(),
+            "jpg", temp);
+
+        Assertions.assertTrue(compareImage(
+            new File("./src/main/resources/rose.jpg"), temp));
     }
+
+    private static boolean compareImage(File fileA, File fileB) throws IOException {
+
+            BufferedImage biA = ImageIO.read(fileA);
+            DataBuffer dbA = biA.getData().getDataBuffer();
+            int sizeA = dbA.getSize();
+
+            BufferedImage biB = ImageIO.read(fileB);
+            DataBuffer dbB = biB.getData().getDataBuffer();
+            int sizeB = dbB.getSize();
+
+            if(sizeA == sizeB)
+            {
+                for(int i=0; i<sizeA; i++)
+                {
+                    if(dbA.getElem(i) != dbB.getElem(i))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
 }
